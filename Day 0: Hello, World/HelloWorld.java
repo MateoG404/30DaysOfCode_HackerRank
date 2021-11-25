79af6483b64f91f686faa2e15153da69ae3b857f
Mateo Gutiérrez Melo <72769799+MateoG404@users.noreply.github.com>
2021-11-25 18:36:14 -0500

    Create HelloWorld.java

---
+++ Day 0: Hello, World/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner entry = new Scanner(System.in); 
+		String string_input;
+    string_input = entry.nextLine();
+    System.out.println("Hello, World.");
+    System.out.print(string_input);
+	}
+}
