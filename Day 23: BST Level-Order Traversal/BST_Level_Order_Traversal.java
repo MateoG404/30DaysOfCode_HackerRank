5cbc7687fb001bab900574b69d3ef00157e78bf9
Mateo Gutiérrez Melo <72769799+MateoG404@users.noreply.github.com>
2021-11-25 19:09:36 -0500

    Create BST_Level_Order_Traversal.java

---
+++ Day 23: BST Level-Order Traversal/BST_Level_Order_Traversal.java
@@ -0,0 +1,14 @@
+
+
+static void levelOrder(Node root){
+    Queue<Node> queue = new LinkedList();
+    queue.add(root);
+    
+    while(!queue.isEmpty()){
+        Node current = queue.remove();
+        System.out.print(current.data+" ");
+        if (current.left!=null) queue.add(current.left);
+        if (current.right!=null) queue.add(current.right);
+    }
+}
+
