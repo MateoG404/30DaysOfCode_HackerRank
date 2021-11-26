0bafe1e5aecc04ea13ed23421c471b31ba0f8fb5
Mateo Gutiérrez Melo <72769799+MateoG404@users.noreply.github.com>
2021-11-25 19:06:51 -0500

    Create Queues_and_Stacks.java

---
+++ Day 18: Queues and Stacks/Queues_and_Stacks.java
@@ -0,0 +1,22 @@
+
+
+public class Solution {
+    Stack<Character> stack = new Stack<>();
+    Queue<Character> queue = new LinkedList<>();
+
+    void pushCharacter(char ch) {
+        stack.push(ch);
+    }
+
+    void enqueueCharacter(char ch) {
+        queue.add(ch);
+    }
+
+    char popCharacter() {
+        return stack.pop();
+    }
+
+    char dequeueCharacter() {
+        return queue.remove();
+    }
+
